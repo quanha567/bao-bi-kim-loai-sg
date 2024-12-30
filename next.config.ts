473ef3389aed2bb1ad52c2a53c4382ae2a5fd17f
SUMMER\quanh <quanhavo005@gmail.com>
2024-12-31 01:37:13 +0700

    change nextjs config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    async rewrites() {
+        return [
+            {
+                destination: 'https://baobikimloaisg.vercel.app/api/:path*',
+                source: '/api/:path*',
+            },
+        ]
+    },
     /* config options here */
     typescript: { ignoreBuildErrors: true },
 }
